9ddba2e9742fe4a1518f34e01383bceebc10eafd
Nathaniel Arnold <nathanielclayarnold@heathers-macbook-air.local>
2017-03-23 12:49:35 -0400

    Initial Commit

---
+++ Student/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Student
+//
+//  Created by Nathaniel Clay Arnold  on 3/23/17.
+//  Copyright © 2017 Nathaniel Clay Arnold . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
